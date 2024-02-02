3c39c0b8a76db84b87f480dac44c1aac30ea83ca
Jonas Peterburs <j.peterburs@protonmail.com>
2024-02-02 20:59:30 +0000

    Add basic dar and light mode

--- js/script.js
+++ js/script.js
@@ -10,3 +10,14 @@ document.addEventListener('DOMContentLoaded', () => {
     h1Element.textContent = 'Nein';
   }
 })
+
+const btn = document.querySelector('#theme-toggle');
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+btn.addEventListener('click', function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle('light-theme');
+  } else {
+    document.body.classList.toggle('dark-theme');
+  }
+});
