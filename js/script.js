517c45aeb011d1f52f8c3e5e281f49001fc8212b
Jonas Peterburs <j.peterburs@protonmail.com>
2024-07-04 19:57:52 +0000

    Remove theme toggle

--- js/script.js
+++ js/script.js
@@ -8,25 +8,4 @@ document.addEventListener('DOMContentLoaded', () => {
   else {
     element.textContent = 'Nein';
   }
-})
-
-const themeToggle = document.querySelector('#theme-toggle');
-const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-const savedTheme = localStorage.getItem('theme');
-
-if (savedTheme) {
-  document.body.classList.add(savedTheme);
-  themeToggle.checked = savedTheme === 'light-theme';
-} else {
-  document.body.classList.add(prefersDarkScheme.matches ? 'dark-theme' : 'light-theme');
-}
-
-themeToggle.addEventListener('change', () => {
-  if (themeToggle.checked) {
-    document.body.classList.replace('dark-theme', 'light-theme');
-    localStorage.setItem('theme', 'light-theme');
-  } else {
-    document.body.classList.replace('light-theme', 'dark-theme');
-    localStorage.setItem('theme', 'dark-theme');
-  }
 });
