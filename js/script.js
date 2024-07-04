38b0ad69e5915f2ed0b2aae325542a4edbfd6843
Jonas Peterburs <j.peterburs@protonmail.com>
2024-07-04 20:05:37 +0000

    Cleanup files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const element = document.querySelector('#result');
 
-  const currentDate = new Date()
+  const currentDate = new Date();
   if (currentDate.getDay() === 3) {
     element.textContent = 'Es ist Mittwoch, meine Kerle';
   }
