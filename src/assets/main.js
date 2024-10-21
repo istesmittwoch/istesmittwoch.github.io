1ccd9406fa33de1c9f44bfb14564de2f8ec5c04a
Jonas Peterburs <j.peterburs@protonmail.com>
2024-10-21 18:35:35 +0000

    Minimize code further

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,11 +1,2 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const element = document.querySelector('.answer');
-
-  const currentDate = new Date();
-  if (currentDate.getDay() === 3) {
-    element.textContent = 'Es ist Mittwoch, meine Kerle';
-  }
-  else {
-    element.textContent = 'Nein';
-  }
-});
+const currentDate = new Date()
+document.getElementById('answer').textContent = currentDate.getDay() === 3 ? 'Es ist Mittwoch, meine Kerle' : 'Nein'
